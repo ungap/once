e44fb4233da46797233ff10aa5a1e4035090f152
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2022-04-05 17:26:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+
+  /*! (c) Andrea Giammarchi - ISC */
+  Function.prototype.once || Object.defineProperty(
+    Function.prototype,
+    'once',
+    {
+      writable: true,
+      configurable: true,
+      value() {
+        let fn = this, execute = true, returned;
+        return function once() {
+          if (execute) {
+            execute = false;
+            returned = fn.apply(this, arguments);
+          }
+          return returned;
+        };
+      }
+    }
+  );
+
+})();
