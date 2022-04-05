142ffa365aa95f22c65944010de6023677a15627
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2022-04-05 19:39:22 +0200

    Same code, better tests

--- test/index.js
+++ test/index.js
@@ -3,7 +3,18 @@ if (Function.prototype.once)
 
 require('../cjs');
 
-const log = console.log.once().bind(console);
+const executions = [];
+const log = function (...args) {
+  const value = Math.random();
+  executions.push([this, value, args]);
+  return value;
+}.once();
 
-log(1);
-log(1);
+const result = log.call(JSON, 1, 2);
+log.call(JSON, 3);
+
+console.assert(executions.length === 1, 'one execution only');
+console.assert(executions[0][0] === JSON, 'correct context');
+console.assert(executions[0][1] === result, 'expected result');
+console.assert(log.call(JSON, 4) === result, 'expected result again');
+console.assert(executions[0][2].join('-') === '1-2', 'expected args');
